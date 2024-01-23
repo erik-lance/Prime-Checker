9fe72504da7d66d3cc2175f959605569005cb272
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-01-23 23:43:57 +0800

    Refactored user input to catch enter

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<string>
 #include <thread>
 
 // Constants
@@ -6,17 +7,16 @@ const int MAX_LIMIT = 1000000000;
 const int MAX_THREADS = 100;
 
 // Function prototypes
+int user_input(int& limit, int& threads);
 int validate_inputs(int limit, int threads);
 
 int main()
 {
     int LIMIT = 0;
     int n_threads = 1;
-
-    std::cout << "Enter the upper bound for the prime numbers: ";
-    std::cin >> LIMIT;
-    std::cout << "Enter the number of threads: ";
-    std::cin >> n_threads;
+    
+    // Get user input
+    if (!user_input(LIMIT, n_threads)) return 0;
 
     // Input validation
     if (!validate_inputs(LIMIT, n_threads)) return 0;
@@ -29,6 +29,47 @@ int main()
     */
 
     return 0;
+}
+
+/**
+ * This function collects the user input for the upper bound and number of threads.
+ * @param limit The upper bound for the prime numbers.
+ * @param threads The number of threads to use.
+ * @return 1 if the inputs are valid, 0 otherwise.
+*/
+int user_input(int& limit, int& threads)
+{
+    std::string input;
+
+    std::cout << "Enter the upper bound for the prime numbers: ";
+    std::getline(std::cin, input);
+    if (input.empty()) {
+        std::cerr << "No input provided" << std::endl;
+        return 0;
+    } else {
+        try {
+            limit = std::stoi(input);
+        } catch (std::invalid_argument& e) {
+            std::cerr << "Invalid input provided" << std::endl;
+            return 0;
+        }
+    }
+
+    std::cout << "Enter the number of threads: ";
+    std::getline(std::cin, input);
+    if (input.empty()) {
+        std::cerr << "No input provided" << std::endl;
+        return 0;
+    } else {
+        try {
+            threads = std::stoi(input);
+        } catch (std::invalid_argument& e) {
+            std::cerr << "Invalid input provided" << std::endl;
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 /**
